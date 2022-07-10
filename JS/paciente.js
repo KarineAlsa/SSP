230cafe4f81dc509c2a0a94358e7bc4177feb101
saremana <67878261+saremana@users.noreply.github.com>
2022-07-09 20:08:50 -0500

    Delete JS directory

--- JS/paciente.js
+++
@@ -1,5 +0,0 @@
-listaUsuario=JSON.parse(localStorage.getItem("lista"));
-console.log(listaUsuario);
-var textoId = listaUsuario[0].nombre+ " " + listaUsuario[0].apellidop+ " " + listaUsuario[0].apellidom;
-document.getElementById('Nombre').innerHTML=textoId;
-                    
